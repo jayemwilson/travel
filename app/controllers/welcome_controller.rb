ae2fcff158532113898f7d55141460cc68294ca0
jeanette Wilson <jayem.wilson@gmail.com>
2017-06-28 21:18:37 -0400

    travel

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,11 @@
+class WelcomeController < ApplicationController
+  def index
+    
+    @homeland = 'Bronx, NY'
+  
+    @countries = ['Greece','Tahiti','Italy','Aruba']
+
+  end
+  def params
+  end
+end
